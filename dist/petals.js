569e6d8f13ca5999200dfef5f2be218e53552fdf
darrensheng1325 <darrensheng1325@gmail.com>
2025-11-29 15:52:12 -0800

    added lightning cutter

--- dist/petals.js
+++ dist/petals.js
@@ -537,6 +537,35 @@ const BASE_PETAL_CONFIGS = {
 </svg>`,
         isAdminPetal: false
     },
+    lightning_cutter: {
+        name: "Lightning Cutter Petal",
+        damage: 30,
+        health: 1,
+        size: 7.0,
+        cooldown: 1,
+        description: "A cutter that deals damage to the target",
+        knockback: 5,
+        color: "#000000",
+        actions: "delay 1000; lightning 1000; break;",
+        count: 1,
+        range: 0.0,
+        image: `<svg width="32" height="32" viewBox="-40 -40 80 80" xmlns="http://www.w3.org/2000/svg">
+  <path fill="#00ffff" fill-rule="evenodd" d="
+    M 25 0 A 25 25 0 1 0 -25 0 A 25 25 0 1 0 25 0
+    M 24.749 24.749
+    Q 9.899 23.899 0 35
+    Q -9.899 23.899 -24.749 24.749
+    Q -23.899 9.899 -35 0
+    Q -23.899 -9.899 -24.749 -24.749
+    Q -9.899 -23.899 0 -35
+    Q 9.899 -23.899 24.749 -24.749
+    Q 23.899 -9.899 35 0
+    Q 23.899 9.899 24.749 24.749
+    Z" 
+  />
+</svg>`,
+        isAdminPetal: false
+    },
     sparkle: {
         name: "Sparkle Petal",
         damage: 9999999999,
