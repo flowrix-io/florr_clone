0d04958de69c7feeef549df886433706b9d530d1
darrensheng1325 <darrensheng1325@gmail.com>
2025-12-10 21:21:53 -0800

    changed blood leaf

--- src/petal_action/blood_leaf.action.ts
+++ src/petal_action/blood_leaf.action.ts
@@ -1,9 +1,7 @@
 export const blood_leaf_action = `
 if memory:player:extended == 1;
 explode 100;
+heal -1
 set_memory blood_leaf_count memory:petal:count:blood_leaf;
-if memory:blood_leaf_count > 0;
-set_petal_size memory:blood_leaf_count;
-endif;
 endif;
 `;
